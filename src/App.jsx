d1abc5df5b0dafb57986bb759d34155b893f9e87
GURUVEDHANTH S <officialvedhanth@gmail.com>
2023-11-24 15:42:52 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,23 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+  const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridGap: '56px',
+  };
+
+  return (
+    <div>
+      <h1 className='heading' style={{textAlign:"center"}}>Kalvium gallary</h1>
+      <div className='gridContainerStyle'>
+      <img src={elephant} alt="Image"/>
+      <img src={elephant} alt="Image"/>
+      <img src={elephant} alt="Image"/>
+      <img src={elephant} alt="Image"/>
+      </div>
+    </div>
+  );
 }
 
 export default App;
